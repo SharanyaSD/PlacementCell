2e4c7d1322e43aa7581f8bfd77a87f68a17e677f
Sharanya <datrangesharanya@gmail.com>
2024-03-31 00:51:51 +0530

    Displayed logged user

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
+  const [userDetails, setuserDetails] = useState<any>(null);
+  useEffect(() => {
+    const loggedUserDetails = localStorage.getItem("userDetails");
+    if (loggedUserDetails) {
+      const parsedUserDetails = JSON.parse(loggedUserDetails);
+      setuserDetails(parsedUserDetails);
+    }
+  }, []);
   return (
     <div>
-      <h1> Account</h1>
+      <h1> User Account </h1>
+      {userDetails && (
+        <div>
+          <p>
+            <strong>Name :</strong> {userDetails.first_name}{" "}
+            {userDetails.last_name}
+          </p>
+          <p>
+            <strong>Email: </strong> {userDetails.email}{" "}
+          </p>
+          <p>
+            <strong>Batch: </strong> {userDetails.batch}{" "}
+          </p>
+          <p>
+            <strong>Branch: </strong> {userDetails.branch}{" "}
+          </p>
+          <p>
+            <strong>Created at : </strong> {userDetails.created_at}{" "}
+          </p>
+          <p>
+            <strong>LinkedIn: </strong> {userDetails.linkedin}{" "}
+          </p>
+
+          <p>
+            <strong>Updated at: </strong> {userDetails.updated_at}{" "}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
