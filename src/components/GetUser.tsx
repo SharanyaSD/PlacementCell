f09746b8e8f61a513bab71ed7818d1ab28a46851
Sharanya <datrangesharanya@gmail.com>
2024-04-02 17:41:16 +0530

    fill user details

---
+++ src/components/GetUser.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { User } from "../api/auth";
+import API_BASE_URL from "../api/apiConfig";
+import axios from "axios";
+import storage from "../utilities/storage";
+
+const GetUser = () => {
+  const { email } = useParams();
+  const [user, setUser] = useState<User>();
+  //   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (email) {
+      axios({
+        method: "GET",
+        url: `${API_BASE_URL}/users?email=${email}`,
+        headers: { Authorization: `Bearer ${storage.getToken()}` },
+      })
+        .then((res) => {
+          setUser(res.data);
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [email]);
+
+  // const navigateCreate = () => {
+  //   navigate("/users");
+  // };
+  return (
+    <div>
+      <h1> Created user - </h1>
+      {user && (
+        <div>
+          <p>
+            <strong>Name :</strong> {user.first_name} {user.last_name}
+          </p>
+          <p>
+            <strong>Email: </strong> {user.email}{" "}
+          </p>
+          <p>
+            <strong>Batch: </strong> {user.batch}{" "}
+          </p>
+          <p>
+            <strong>Branch: </strong> {user.branch}{" "}
+          </p>
+          <p>
+            <strong>LinkedIn: </strong> {user.linkedin}{" "}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GetUser;
