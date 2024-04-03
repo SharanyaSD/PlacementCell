0cada49f9112b0cfcbe0aba497afd09c7085df5b
Sharanya <datrangesharanya@gmail.com>
2024-04-04 00:47:35 +0530

    on admin/tpo  login

--- src/components/AdminAccount.tsx
+++ src/components/AdminAccount.tsx
@@ -18,13 +18,29 @@ const AdminAccount = () => {
     <div>
       <h1> ADMIN PAGE</h1>
       <div className="userform">
-        <button onClick={navigateCreateUser}>Create new user</button>
+        <button
+          className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          onClick={navigateCreateUser}
+        >
+          Create new user
+        </button>
       </div>
       <div className="companyForm">
-        <button onClick={navigateCompany}>Add new company </button>
+        <button
+          className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          onClick={navigateCompany}
+        >
+          Add new company{" "}
+        </button>
       </div>
       <div>
-        <button onClick={navigateAllCompanies}> All Companies </button>
+        <button
+          className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          onClick={navigateAllCompanies}
+        >
+          {" "}
+          All Companies{" "}
+        </button>
       </div>
     </div>
   );
