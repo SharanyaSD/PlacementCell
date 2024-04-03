040cda3b7b1fed0dc095c3fe24de989a875cd813
Sharanya <datrangesharanya@gmail.com>
2024-04-03 17:36:38 +0530

    create Opportunities

--- src/components/CreateOpportunityForm.tsx
+++ src/components/CreateOpportunityForm.tsx
@@ -1,6 +1,6 @@
 import { useFormik } from "formik";
 import React from "react";
-import { createOpportunity, Opportunity } from "../api/auth";
+import { createOpportunity } from "../api/auth";
 import { useNavigate, useParams } from "react-router-dom";
 
 // interface CreateOpportunityFormProps {
@@ -13,9 +13,10 @@ const CreateOpportunityForm = () => {
   const { id } = useParams();
   console.log("in ", id);
   const create_opportunity = async (values: any) => {
-    console.log("in create", values);
+    // console.log("in create", values);
     try {
-      const response = await createOpportunity({ values });
+      console.log("try")
+      const response = await createOpportunity(values);
       console.log(response.data);
     } catch (error) {
       console.error("Error creating opportunity:", error);
@@ -32,11 +33,13 @@ const CreateOpportunityForm = () => {
       package: 0,
       start_date: "",
       end_date: "",
+
     },
     onSubmit: (values) => {
+      // console.log(values.company_id);
       create_opportunity(values);
       // navigate(`/opportunities/${props.companyId}`);
-      navigate(`/opportunities/${values.company_id}`);
+      navigate(`/opportunities/${id}`);
     },
     validate: (values) => {
       const errors: any = {};
@@ -64,30 +67,44 @@ const CreateOpportunityForm = () => {
 
   return (
     <div>
-      <h2>Create New Opportunity - </h2>
-      <form onSubmit={formik.handleSubmit}>
-        <div>
-          <label>Status:</label>
+      <h2>Create New Opportunity</h2>
+      <form onSubmit={formik.handleSubmit} className="max-w-md mx-auto">
+        <div className="relative z-0 w-full mb-5 group">
+          <label
+            htmlFor="status"
+            className="text-sm text-gray-500 dark:text-gray-400"
+          >
+            Status:
+          </label>
           <input
             type="text"
             name="status"
+            id="status"
             value={formik.values.status}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           />
           {formik.touched.status && formik.errors.status ? (
             <div>{formik.errors.status}</div>
           ) : null}
         </div>
 
-        <div>
-          <label>No. of Applications:</label>
+        <div className="relative z-0 w-full mb-5 group">
+          <label
+            htmlFor="no_of_applications"
+            className="text-sm text-gray-500 dark:text-gray-400"
+          >
+            No. of Applications:
+          </label>
           <input
             type="number"
             name="no_of_applications"
+            id="no_of_applications"
             value={formik.values.no_of_applications}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           />
           {formik.touched.no_of_applications &&
           formik.errors.no_of_applications ? (
@@ -95,70 +112,105 @@ const CreateOpportunityForm = () => {
           ) : null}
         </div>
 
-        <div>
-          <label>Designation:</label>
+        <div className="relative z-0 w-full mb-5 group">
+          <label
+            htmlFor="designation"
+            className="text-sm text-gray-500 dark:text-gray-400"
+          >
+            Designation:
+          </label>
           <input
             type="text"
             name="designation"
+            id="designation"
             value={formik.values.designation}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           />
           {formik.touched.designation && formik.errors.designation ? (
             <div>{formik.errors.designation}</div>
           ) : null}
         </div>
 
-        <div>
-          <label>Skillset:</label>
+        <div className="relative z-0 w-full mb-5 group">
+          <label
+            htmlFor="skillset"
+            className="text-sm text-gray-500 dark:text-gray-400"
+          >
+            Skillset:
+          </label>
           <input
             type="text"
             name="skillset"
+            id="skillset"
             value={formik.values.skillset}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           />
           {formik.touched.skillset && formik.errors.skillset ? (
             <div>{formik.errors.skillset}</div>
           ) : null}
         </div>
 
-        <div>
-          <label>Package:</label>
+        <div className="relative z-0 w-full mb-5 group">
+          <label
+            htmlFor="package"
+            className="text-sm text-gray-500 dark:text-gray-400"
+          >
+            Package:
+          </label>
           <input
             type="text"
             name="package"
+            id="package"
             value={formik.values.package}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           />
           {formik.touched.package && formik.errors.package ? (
             <div>{formik.errors.package}</div>
           ) : null}
         </div>
 
-        <div>
-          <label>Start Date:</label>
+        <div className="relative z-0 w-full mb-5 group">
+          <label
+            htmlFor="start_date"
+            className="text-sm text-gray-500 dark:text-gray-400"
+          >
+            Start Date:
+          </label>
           <input
             type="date"
             name="start_date"
+            id="start_date"
             value={formik.values.start_date}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           />
           {formik.touched.start_date && formik.errors.start_date ? (
             <div>{formik.errors.start_date}</div>
           ) : null}
         </div>
 
-        <div>
-          <label>End Date:</label>
+        <div className="relative z-0 w-full mb-5 group">
+          <label
+            htmlFor="end_date"
+            className="text-sm text-gray-500 dark:text-gray-400"
+          >
+            End Date:
+          </label>
           <input
             type="date"
             name="end_date"
+            id="end_date"
             value={formik.values.end_date}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           />
           {formik.touched.end_date && formik.errors.end_date ? (
             <div>{formik.errors.end_date}</div>
@@ -166,8 +218,8 @@ const CreateOpportunityForm = () => {
         </div>
 
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Create Opportunity
         </button>
