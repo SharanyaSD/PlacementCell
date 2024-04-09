88998497f1132c9cef244cdbce4bd1312f230f4b
Sharanya <datrangesharanya@gmail.com>
2024-04-09 13:12:24 +0530

    company updating form

--- src/components/UpdateCompanyForm.tsx
+++ src/components/UpdateCompanyForm.tsx
@@ -38,99 +38,69 @@ const UpdateCompanyForm = () => {
   });
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-        backgroundColor: "#f0f0f0",
-      }}
-    >
-      <div
-        style={{
-          width: "500px",
-          height: "450px",
-          backgroundColor: "#ffffff",
-          padding: "20px",
-          borderRadius: "5px",
-          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-          onSubmit={formik.handleSubmit}
+    <div>
+      <h1>Update Company Details</h1>
+      <form onSubmit={formik.handleSubmit} className="max-w-md mx-auto">
+        <div className="relative z-0 w-full mb-5 group">
+          <label
+            htmlFor="name"
+            className="text-sm text-gray-500 dark:text-gray-400"
+          >
+            Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="information"
+            className="text-sm text-gray-500 dark:text-gray-400"
+          >
+            Information:
+          </label>
+          <input
+            type="text"
+            id="information"
+            name="information"
+            value={formik.values.information}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="website"
+            className="text-sm text-gray-500 dark:text-gray-400"
+          >
+            Website:
+          </label>
+          <input
+            type="text"
+            id="website"
+            name="website"
+            value={formik.values.website}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+        </div>
+        <div className="pt-10"></div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={onSubmit}
         >
-          <div>
-            <h1>Update Company Details</h1>
-            <form onSubmit={formik.handleSubmit} className="max-w-md mx-auto">
-              <div className="relative z-0 w-full mb-5 group">
-                <label
-                  htmlFor="name"
-                  className="text-sm text-gray-500 dark:text-gray-400"
-                >
-                  Name:
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="information"
-                  className="text-sm text-gray-500 dark:text-gray-400"
-                >
-                  Information:
-                </label>
-                <input
-                  type="text"
-                  id="information"
-                  name="information"
-                  value={formik.values.information}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="website"
-                  className="text-sm text-gray-500 dark:text-gray-400"
-                >
-                  Website:
-                </label>
-                <input
-                  type="text"
-                  id="website"
-                  name="website"
-                  value={formik.values.website}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                />
-              </div>
-              <div className="pt-10"></div>
-              <button
-                type="submit"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Update Company
-              </button>
-            </form>
-          </div>
-        </Box>
-      </div>
+          Update Company
+        </button>
+      </form>
     </div>
   );
 };
